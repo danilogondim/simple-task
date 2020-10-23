b16fd041d8b31186a39b4fd92586b421bc153262
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-23 20:05:50 +0000

    Add the updateUser function

--- server/models/index.js
+++ server/models/index.js
@@ -15,30 +15,64 @@ module.exports = (db) => {
     const query = {
       text: `SELECT * FROM users WHERE email = $1`,
       values: [email]
-    }
+    };
 
     return db
       .query(query)
       .then(result => result.rows[0])
       .catch((err) => err);
-  }
+  };
 
   // is_tasker, is_available and vehicle will not be filled in this step. Becoming a tasker is considered an update
   const addUser = (firstName, lastName, phone, email, password, address, coordinates, photoUrl) => {
     const query = {
       text: `INSERT INTO users (first_name, last_name, phone, email, password, address, coordinates, photo_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
       values: [firstName, lastName, phone, email, password, address, coordinates, photoUrl]
-    }
+    };
 
     return db.query(query)
       .then(result => result.rows[0])
       .catch(err => err);
-  }
+  };
 
+
+  // used if a user wants to update some information or decides to become a tasker
+  const updateUser = userObject => {
+    // a query will only be build and run if an user id was provided and the passed object has more than the id key
+    if (userObject.id && Object.keys(userObject).length > 1) {
+
+      // all the accepted fields in our database
+      const userFields = ['first_name', 'last_name', 'phone', 'email', 'password', 'address', 'coordinates', 'is_tasker', 'photo_url', 'is_available', 'vehicle'];
+
+      // the sql query and the values array should be build dynamically
+      let text = `UPDATE users \nSET `;
+      const values = [userObject.id];
+
+      for (const field of userFields) {
+        if (userObject[field]) {
+          values.push(userObject[field]);
+          text += `${field} = $${values.length},\n`;
+        }
+      }
+      text = text.slice(0, -2) + "\nWHERE users.id = $1 \nRETURNING *;";
+
+      return db.query(text, values)
+        .then(result => {
+          if (result.rows[0]) {
+            return result.rows[0];
+          }
+          throw 'The user id does not exist';
+        })
+        .catch(err => err);
+    }
+
+    throw 'It seems that the user id is missing or there are no fields to update';
+  };
 
   return {
     getUsers,
     getUserByEmail,
-    addUser
+    addUser,
+    updateUser
   };
 };
