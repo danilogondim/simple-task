9c1ccb0da1c2a67f9c5bee347a92c8a243f9d4e8
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-23 00:11:44 +0000

    Add categories and services schemas

---
+++ server/db/schema/03_services.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS services CASCADE;
+CREATE TABLE services (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
+);
