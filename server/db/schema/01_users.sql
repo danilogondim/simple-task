e120800707edcddce2d11d27003f4278e8bc271e
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-23 00:06:09 +0000

    Add empty schema files to organize order. Refactor users schema and seed to our needs

--- server/db/schema/01_users.sql
+++ server/db/schema/01_users.sql
@@ -1,8 +1,23 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
-    id SERIAL PRIMARY KEY NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  is_tasker BOOLEAN DEFAULT 'f',
+  photo_url TEXT,
+  is_available BOOLEAN,
+  vehicle VARCHAR(255),
+  -- if the user is a tasker, it should have photo, availability (true or false) and vehicle:
+  CONSTRAINT if_tasker_then_photo_url_is_not_null 
+    CHECK ( NOT (is_tasker AND photo_url IS NULL) ),
+
+  CONSTRAINT if_tasker_then_is_available_is_not_null 
+    CHECK ( NOT (is_tasker AND is_available IS NULL) ),
+
+  CONSTRAINT if_tasker_then_vehicle_is_not_null 
+    CHECK ( NOT (is_tasker AND vehicle IS NULL) )
 );
--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,18 +1,16 @@
+-- users
 INSERT INTO
-users(first_name, last_name, email, password)
+users(first_name, last_name, phone, email, password, address)
 VALUES
-    ('Mario', 'Bros', 'mario@nintendo.com', 'test'),
-    ('Luigi', 'Bros', 'luigi@nintendo.com', 'test'),
-    (
-        'Princess',
-        'Peach',
-        'peach@nintendo.com',
-        'test'
-    ),
-    (
-        'Princess',
-        'Daisy',
-        'daisy@nintendo.com',
-        'test'
-    ),
-    ('Donkey', 'Kong', 'donkey@nintendo.com', 'test');
+ ('Mario', 'Bros', '4161234567', 'mario@nintendo.com', 'test', '123 random street'),
+ ('Luigi', 'Bros', '6477654321', 'luigi@nintendo.com', 'test', '123 random street'),
+ ('Princess', 'Peach', '4162222222', 'peach@nintendo.com', 'test', '123 random street'),
+ ('Princess', 'Daisy', '6479999999', 'daisy@nintendo.com', 'test', '123 random street' ),
+ ('Donkey', 'Kong', '6474567890', 'donkey@nintendo.com', 'test', '123 random street');
+
+-- taskers
+INSERT INTO
+users(first_name, last_name, phone, email, password, address, is_tasker, photo_url, is_available, vehicle)
+VALUES
+ ('Hammer', 'Bros', '4167653112', 'hammer@nintendo.com', 'test', '123 random street', 't', 'https://www.mariowiki.com/images/e/e3/NSMBU_Hammer_Bro_Artwork.png', 't', 'bicycle'),
+ ('Lakitu', 'Koopa', '6477654321', 'lakitu@nintendo.com', 'test', '123 random street', 't', 'https://upload.wikimedia.org/wikipedia/en/2/2d/Lakitu.png', 'f', 'cloud');
