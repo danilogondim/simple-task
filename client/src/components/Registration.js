274cb6ba189e91a9c0abd7a7e2f51792544cd65b
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-10-24 14:01:30 -0400

    Add error messages and working with axios

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Registration.scss';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
+
 
 export default function Registration() {
 
 
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => {
-    console.log(data)
+  const onSubmit = (user) => {
+
+    axios
+    .post('/api/users/', user)
+    .then(() => console.log('User created'))
+    .catch(err => {
+      console.error(err);
+    });
+
+    // console.log(data)
+    // res.send (data)
   }
 
 
@@ -20,26 +31,42 @@ export default function Registration() {
 
       <h2>User: </h2>
         <form className='Registration-form' onSubmit={handleSubmit(onSubmit)}>
-          <label>First Name*:</label>
-          <input type="text" name="first_name" ref={register({ required: true})} /><br />
+          
+          <label>First Name:</label>
+          <input type="text" name="first_name" ref={register({ required: true})} />
+          {errors.first_name && <p> This is a mandatory field. </p>}
+          <br />
+          
 
-          <label>Last Name*:</label>
-          <input type="text" name="last_name" ref={register({ required: true})}  /><br />
+          <label>Last Name:</label>
+          <input type="text" name="last_name" ref={register({ required: true})}  />
+          {errors.last_name && <p> This is a mandatory field. </p>}
+          <br />
+         
 
-          <label>Phone*:</label>
-          <input type="tel" name="phone" ref={register({ required: true})}  /><br />
+          <label>Phone:</label>
+          <input type="tel" name="phone" ref={register({ required: true})}  />          
+          {errors.phone && <p> This is a mandatory field. </p>}
+          <br />
 
-          <label>Email*:</label>
-          <input type="email" name="email" ref={register({ required: true})}  /><br />
+          <label>Email:</label>
+          <input type="email" name="email" ref={register({ required: true})}  />
+          {errors.email && <p> This is a mandatory field. </p>}
+          <br />
 
-          <label>Password*:</label>
+          <label>Password:</label>
           <input type="password" name="password" ref={register({ required: true, minLength: 6})}  /><br />
-          <label>Address*:</label>
+          {errors.password && errors.password.type === "required" && <p> This is a mandatory field. </p>}
+          {errors.password && errors.password.type === "minLength" && <p> Password must have at least 6 characters. </p>}
+
+
+          <label>Address:</label>
           <input type="text" name="address" ref={register({ required: true})}  /><br />
+          {errors.address && <p> This is a mandatory field. </p>}
 
           <label>Photo:</label>
           <input type="text" name="photo_url" ref={register({ required: false})}  /><br />
-          
+
           <input type="submit" name="submit_register" />
         </form>
 
