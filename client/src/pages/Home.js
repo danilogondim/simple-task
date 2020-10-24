3e1da4d562d8a0b138062189fa94dc764e667a03
vinay baswa <vinaybaswa@gmail.com>
2020-10-24 17:51:26 -0400

    clickable links from home/category to services

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import useCategories from '../hooks/useCategories.js'
 
 export default function Home() {
@@ -10,7 +11,9 @@ export default function Home() {
         <br></br>
         {category.category_description}
         <br></br>
+        <Link to={`categories/${category.category_id}/services`}>
         <img src={category.category_thumbnail} alt={category.category}/>
+        </Link>
         <br></br>
         <br></br>
       </li>
@@ -18,7 +21,7 @@ export default function Home() {
 
   return (
     <div className="App" >
-      <h2> Categories </h2>
+      <h2> Select a Category </h2>
       <ul> {categories} </ul>
     </div >
   );
