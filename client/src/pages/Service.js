99bc4fb3380f352e4e211f4b8dbf15c74d04fe29
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-25 18:53:45 +0000

    Render taskers information in a service page

--- client/src/pages/Service.js
+++ client/src/pages/Service.js
@@ -6,10 +6,40 @@ import useTaskers from '../hooks/useTaskers'
 export default function Service() {
   const { id } = useParams();
   const { state } = useTaskers(id);
+  const taskers = !state.taskers ? "" : state.taskers.map(tasker => {
+    return (
+      <li key={tasker.id} >
+        {tasker.name}
+        <br></br>
+        {tasker.first_name}
+        <br></br>
+        {tasker.last_name}
+        <br></br>
+        {tasker.phone}
+        <br></br>
+        {tasker.address}
+        <br></br>
+        {tasker.coordinates}
+        <br></br>
+        {tasker.email}
+        <br></br>
+        <img src={tasker.photo_url} alt={tasker.first_name + " " + tasker.last_name} />
+        <br></br>
+        {tasker.vehicle}
+        <br></br>
+        {tasker.hourly_rate}
+        <br></br>
+        {tasker.user_rating}
+        <br></br>
+        {/* {tasker.availability} */}
+        <br></br>
+      </li>
+    )
+  })
   return (
     <div>
-      <h1>Categories ➟ Category ➟ Services ➟ Service ➟ taskers</h1>
-      <h1>{id}</h1>
+      <h2> Select a Tasker </h2>
+      <ul> {taskers} </ul>
     </div>
   );
 }
