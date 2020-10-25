f93a74e1baa28933ea63d93ed8390991960ae1cb
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-25 20:26:53 +0000

    Comment out unnecessary information and wrap takers inside an article

--- client/src/components/TaskerListItem.js
+++ client/src/components/TaskerListItem.js
@@ -4,10 +4,10 @@ export default function TaskerListItem(props) {
   const { id,
     first_name,
     last_name,
-    phone,
-    address,
+    // phone,
+    // address,
     coordinates,
-    email,
+    // email,
     photo_url,
     vehicle,
     hourly_rate,
@@ -15,30 +15,25 @@ export default function TaskerListItem(props) {
     availability
   } = props.tasker;
   return (
-    <li key={id} >
-      {first_name}
-      <br></br>
-      {last_name}
-      <br></br>
-      {phone}
-      <br></br>
-      {address}
-      <br></br>
-      {coordinates}
-      <br></br>
-      {email}
-      <br></br>
-      <img src={photo_url} alt={first_name + " " + last_name} />
-      <br></br>
-      {vehicle}
-      <br></br>
-      {hourly_rate}
-      <br></br>
-      {user_rating}
-      <br></br>
-      {/* {availability} */}
-      <br></br>
-    </li>
-
+    <article className='tasker'>
+      <li key={id} >
+        {first_name}
+        <br></br>
+        {last_name}
+        <br></br>
+        {coordinates}
+        <br></br>
+        <img src={photo_url} alt={first_name + " " + last_name} />
+        <br></br>
+        {vehicle}
+        <br></br>
+        {hourly_rate}
+        <br></br>
+        {user_rating}
+        <br></br>
+        {/* {availability} */}
+        <br></br>
+      </li>
+    </article >
   );
 };
