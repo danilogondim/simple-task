374bc455289d34b4eb3a241de897589d7d917b37
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-26 23:48:33 +0000

    Better format tasker information

--- client/src/components/TaskerListItem.js
+++ client/src/components/TaskerListItem.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Rating } from '@material-ui/lab';
 import './TaskerListItem.scss';
-
+import { AirportShuttle, LocalShipping, DriveEta, DirectionsBike, SportsMotorsports, DirectionsTransit } from '@material-ui/icons';
+// van: <AirportShuttle/>
+// truck: <LocalShipping/>
+// car: <DriveEta/>
+// bicycle: <DirectionsBike/>
+// motorcycle: <SportsMotorsports/>
+// public: <DirectionsTransit/>
 export default function TaskerListItem(props) {
   const { id,
     first_name,
@@ -20,27 +26,31 @@ export default function TaskerListItem(props) {
       <li key={id} >
         <img className="card-img-top" src={photo_url} alt={first_name + " " + last_name} />
         <div className="card-body">
-
-          <br></br>
-          {first_name + ' ' + last_name}
-          <br></br>
-          {user_rating === null && <p>New Tasker!</p>}
-          {user_rating !== null &&
-            <>
-              <Rating
-                name="half-rating-read"
-                defaultValue={Number(user_rating).toFixed(2)}
-                precision={0.5}
-                readOnly
-                className="rating"
-              />
-              <br></br>
-            </>}
-          {'Hourly rate: $' + (hourly_rate / 100).toFixed(2)}
-          <br></br>
-          {'Vehicle: ' + vehicle}
-          <br></br>
-          <br></br>
+          <div className="rating">
+            {user_rating === null && <p>New Tasker!</p>}
+            {user_rating !== null &&
+              <>
+                <Rating
+                  name="half-rating-read"
+                  defaultValue={Number(user_rating).toFixed(2)}
+                  precision={0.5}
+                  readOnly
+                  className="rating-result"
+                />
+              </>
+            }
+          </div>
+          <h5>
+            {first_name + ' ' + last_name}
+          </h5>
+          <div className="price-vehicle">
+            <span>
+              {'$' + (hourly_rate / 100).toFixed(2) + '/h'}
+            </span>
+            <span>
+              {'Vehicle: ' + vehicle}
+            </span>
+          </div>
         </div>
       </li>
     </article >
