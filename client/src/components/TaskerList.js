c78e11cef38900cd3fd7c066e5a63f5ba96220c0
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-26 20:52:38 +0000

    Add a state to control button value and make it able to toggle the state

--- client/src/components/TaskerList.js
+++ client/src/components/TaskerList.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './TaskerList.scss';
 
 import TaskerListItem from './TaskerListItem';
 
 export default function TaskerList(props) {
+
+  const [value, setValue] = useState('rating')
+  const handleClick = () =>{
+    if (value === 'rating') {
+      setValue('price');
+    } else {
+      setValue('rating');
+    }
+  };
+
   const taskers = !props.taskers ? "" : props.taskers.map(tasker => {
     return (
       <TaskerListItem
@@ -17,7 +27,7 @@ export default function TaskerList(props) {
     <section className="taskers">
       <div className="sort">
         <h5>Sorted By: </h5>
-        <button>price/ratings</button>
+        <button onClick={handleClick}>{value}</button>
       </div>
       <h4 className="taskers__header text--light">Select a Tasker</h4>
       <ul className="taskers__list">
