c9ccd93db93b414c12f5a999fcb796d91788708e
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-27 15:21:27 -0400

    Add a basic component to show up with tasker detail whenever a tasker is selected (still do not display information)

---
+++ client/src/components/TaskerDetail.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './TaskerDetail.scss';
+
+
+export default function TaskerDetail(props) {
+  return <h1 onClick={props.setTasker}>A tasker has been selected!!!!!</h1>
+};
--- client/src/pages/Service.js
+++ client/src/pages/Service.js
@@ -3,7 +3,9 @@ import { useParams } from "react-router-dom";
 import useServiceData from '../hooks/useServiceData';
 import Filter from '../components/Filter';
 import TaskerList from '../components/TaskerList';
+import TaskerDetail from '../components/TaskerDetail';
 import taskersFilter from '../helpers/taskersFilter';
+import { SET_TASKER } from '../reducer/data_reducer';
 import './Service.scss';
 
 export default function Service() {
@@ -18,6 +20,7 @@ export default function Service() {
       <main className="selection_page">
         <Filter dispatch={dispatch} day={day} range={range} />
         <TaskerList dispatch={dispatch} service={service} taskers={filteredTaskers} />
+        {tasker && <TaskerDetail setTasker={() => dispatch({ type: SET_TASKER, tasker: null })} />}
       </main>
     </>
   );
