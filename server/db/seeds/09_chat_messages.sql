4da93fc216c41ddfc90931824f601bbd625851da
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-30 16:41:21 -0400

    Add more chat seeds

--- server/db/seeds/09_chat_messages.sql
+++ server/db/seeds/09_chat_messages.sql
@@ -17,7 +17,7 @@ VALUES
   },
   {
     "sender_id":1,
-    "sent_at":"2020-10-22 08:05:00",
+    "sent_at":"2020-10-22 08:02:00",
     "received_at":"2020-10-22 08:15:00",
     "read_at":"2020-10-22 08:20:00",
     "message":"I am wondering if I send you a picture if you are able to estimate how much it will cost me apart from your service and how long it could take"
@@ -29,4 +29,52 @@ VALUES
     "read_at":"2020-10-22 08:23:00",
     "message":"Hello, Daniel! Sure, I''ve done that before with other clients and it worked well. I just need to know about your expectations so I can make a reasonable quote"
   }
-]')
+]'),
+(1, 11, 
+'[
+  {
+    "sender_id":1,
+    "sent_at":"2020-10-22 08:04:00",
+    "received_at":"2020-10-22 08:07:00",
+    "read_at":"2020-10-22 08:10:00",
+    "message":"Hi Elizabeth! I saw that you are offering landscaping services. I have a small garden that I would like to renovate"
+  },
+  {
+    "sender_id":1,
+    "sent_at":"2020-10-22 08:05:00",
+    "received_at":"2020-10-22 08:07:00",
+    "read_at":"2020-10-22 08:10:00",
+    "message":"I am wondering if I send you a picture if you are able to estimate how much it will cost me apart from your service and how long it could take"
+  },
+  {
+    "sender_id":11,
+    "sent_at":"2020-10-22 08:14:00",
+    "received_at":"2020-10-22 08:14:42",
+    "read_at":"2020-10-22 08:17:00",
+    "message":"Hello, Daniel! Thank you for contacting me. Sure, I will be glad to help!"
+  }
+]'),
+(2, 9,
+'[
+  {
+    "sender_id":2,
+    "sent_at":"2020-10-22 13:22:00",
+    "received_at":"2020-10-22 13:32:00",
+    "read_at":"2020-10-22 13:33:00",
+    "message":"Hi Cameron! I need something from Walmart. Are you free today?"
+  },
+  {
+    "sender_id":9,
+    "sent_at":"2020-10-22 13:34:00",
+    "received_at":"2020-10-22 13:37:10",
+    "read_at":"2020-10-22 13:37:20",
+    "message":"Hello, Eric! Yes, I am free up to 4 pm. What do you need?"
+  },
+  {
+    "sender_id":2,
+    "sent_at":"2020-10-22 13:38:00",
+    "received_at":"2020-10-22 13:38:05",
+    "read_at":"2020-10-22 13:38:05",
+    "message":"Great! I will book a task for you right now!"
+  }
+]');
