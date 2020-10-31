152c29baa77f878f62eee3c5091539cd63509645
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-30 23:39:29 -0400

    Add toggle chat logic

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ChatBox.scss';
 import ContactList from './ChatBox/ContactList';
 import MessageList from './ChatBox/MessageList';
-import ToggleChat from './ChatBox/ToggleChat';
 import TextField from '@material-ui/core/TextField';
 import useChatBoxData from '../hooks/useChatBoxData';
 
 
 export default function ChatBox() {
   const user = JSON.parse(localStorage.getItem('user'));
+  const [active, setActive] = useState(false);
 
   const id = !user ? '' : user.id;
   const { state, dispatch } = useChatBoxData(id);
@@ -17,14 +17,19 @@ export default function ChatBox() {
 
   return (
 
-    <section className="chat-box">
-      <div className="contact-list">
-        <ContactList chats={chats} dispatch={dispatch} />
-      </div>
-      <MessageList chat={chat} />
-      <TextField id="standard-basic" label="Type a message" />
-      <ToggleChat />
-    </section>
+    <>
+      {user && active &&
+        <section className="chat-box">
+          <div className="contact-list">
+            <ContactList chats={chats} dispatch={dispatch} />
+          </div>
+          <MessageList chat={chat} />
+          <TextField id="standard-basic" label="Type a message" />
+          <button className="toggle-chat" onClick={() => setActive(false)}>Close!</button>
+        </section>
+      }
+      {!active && <button className="toggle-chat" onClick={() => setActive(true)}>Open!</button>}
+    </>
 
   )
 }
