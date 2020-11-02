b26776b67c92e828ee0e949db72d276f63b20f62
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-01 19:31:56 -0500

    Add a footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,18 +3,15 @@ import {Link} from 'react-router-dom';
 import { useHistory } from "react-router-dom";
 import { AppContext } from "../App.js"
 
+import './Footer.scss';
+
 export default function Footer() {
 
   return (
     <>
-    <nav className="">
-
-    </nav>
-
+      <nav className="footer-bar">
+         <p className="foot">Copyright © 2020 Simple Task. All Rights Reserved.</p>
+      </nav>
     </>
-
   )
-
-
- 
 };
