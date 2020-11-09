5cb402d5ffd5dc16f60423be2bdd5b14564f723e
Danilo Gondim <daniloencarnacao@gmail.com>
2020-11-09 16:25:25 -0500

    Add comments to MessageList component to explain the code

--- client/src/components/ChatBox/MessageList.js
+++ client/src/components/ChatBox/MessageList.js
@@ -6,18 +6,21 @@ import classNames from 'classnames';
 export default function MessageList(props) {
   const { chat, contact } = props;
 
+  // create a reference and manage scrolling to always show last message when the chat is opened or a new message arrives
   const bottomRef = useRef();
-
-
   useEffect(() => {
     bottomRef.current.scrollIntoView();
   }, [chat])
 
+  // render each chat message with the corresponding CSS format
   const messages = !chat ? "" : chat.messages.map((message, index) => {
+
+    // identify which CSS style to apply for a specific chat message (msg sent by user <> msg received from contact)
     const messageClass = classNames("text", {
       "user": message.sender_id !== contact,
       "contact": message.sender_id === contact
     });
+    
     return (
       <ListItem alignItems="flex-start" className="message" key={index}>
         <ListItemText className={messageClass}
@@ -28,6 +31,7 @@ export default function MessageList(props) {
     )
   })
 
+  // render all msgs
   return (
     <div className="autoscroll-container">
       <List className="chat-list scroll-list">
